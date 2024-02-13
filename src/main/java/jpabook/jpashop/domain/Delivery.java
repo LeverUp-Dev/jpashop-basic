f8378461c2cf00faa0d3f0af1951f326dbdeb24a
LeverUp-Dev <123243889+LeverUp-Dev@users.noreply.github.com>
2024-02-14 02:00:33 +0900

    실전예제6-값_타입_매핑

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -1,18 +1,21 @@
 package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import static jakarta.persistence.FetchType.*;
 
 @Entity
+@Getter @Setter
 public class Delivery extends BaseEntity {
 
     @Id @GeneratedValue
     private Long id;
 
-    private String city;
-    private String street;
-    private String zipcode;
+    @Embedded
+    private Address address;
+
     private DeliveryStatus deliveryStatus;
 
     @OneToOne(mappedBy = "delivery", fetch = LAZY)
